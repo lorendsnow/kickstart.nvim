98741ef2ac2d7c9627d171a6a75537aaf4cb8d5e
Loren <lorensnow@protonmail.com>
2025-09-28 11:53:49 -0700

    add more snippets

--- snippets/go.lua
+++ snippets/go.lua
@@ -1,9 +1,38 @@
 return {
-  s('err_return', {
-    t { 'if err != nil {', '\treturn nil, err', '}' },
+  -- Error/Ok Handling
+  s('nnilerr', {
+    t { 'if err != nil {', '\t' },
+    i(1, '//action'),
+    t { '', '}' },
   }),
 
-  s('range_arr', {
+  s('herr', {
+    i(1, '_'),
+    t ', err := ',
+    i(2, 'funcCall'),
+    t { '', 'if err != nil {', '\t' },
+    i(3, '//action'),
+    t { '', '}' },
+  }),
+
+  s('iferr', {
+    t 'if err := ',
+    i(1, '[FUNC]'),
+    t { '; err != nil {', '\t' },
+    i(2, '//action'),
+    t { '', '}' },
+  }),
+
+  s('ifok', {
+    t 'if ok := ',
+    i(1, '[FUNC]'),
+    t { '; !ok {', '\t' },
+    i(2, '//action'),
+    t { '', '}' },
+  }),
+
+  -- Loops
+  s('rangearr', {
     t 'for ',
     i(1, 'i'),
     t ', ',
@@ -30,5 +59,57 @@ return {
     t { ' {', '\t' },
     i(6, '//action'),
     t { '', '}' },
+  }),
+
+  -- Functions/Methods
+  s('nfunc', { -- Normal Function
+    t 'func ',
+    i(1, 'FuncName'),
+    t '(',
+    i(2, 'args'),
+    t ') ',
+    i(3, 'ReturnType'),
+    t { ' {', '\t' },
+    i(4, '//action'),
+    t { '', '}' },
+  }),
+
+  s('sfunc', { -- Struct Method
+    t 'func (',
+    i(1, 'receiver'),
+    t ') ',
+    i(2, 'FuncName'),
+    t '(',
+    i(3, 'args'),
+    t ') ',
+    i(4, 'ReturnType'),
+    t { ' {', '\t' },
+    i(5, '//action'),
+    t { '', '}' },
+  }),
+
+  -- Structs
+  s('pubstruct', {
+    t 'type ',
+    i(1, 'StructName'),
+    t { ' struct {', '\t' },
+    i(2, 'Field'),
+    t ' ',
+    i(3, 'Type'),
+    t { '', '}' },
+  }),
+
+  -- Enums
+  s('tenum', { -- Typed Enum
+    t 'type ',
+    i(1, 'Type'),
+    t { ' int', '', '' },
+    t { 'const (', '\t' },
+    i(2, 'Field'),
+    t ' ',
+    rep(1),
+    t ' = iota',
+    i(3),
+    t { '', ')' },
   }),
 }
