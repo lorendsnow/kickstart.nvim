336490636a7457ee6988d42d025698d4a6aef271
Loren Snow <lorensnow@protonmail.com>
2025-10-03 11:26:26 -0700

    add c snippet file

---
+++ snippets/c.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: undefined-global
+
+return {
+  -- Loops
+  s('for (', {
+    t 'for (',
+    i(1, 'int i = 0'),
+    t '; ',
+    i(2, 'i < _'),
+    t '; ',
+    i(3, 'i++'),
+    t { ') {', '\t' },
+    i(4, ''),
+    t { '', '}' },
+  }),
+}
