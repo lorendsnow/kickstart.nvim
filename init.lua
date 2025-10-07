a85bc0741eebfb7c78facfde087dfc99f2101aa2
Loren Snow <lorensnow@protonmail.com>
2025-10-07 14:20:46 -0700

    add C snippets

--- init.lua
+++ init.lua
@@ -112,6 +112,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Reload luasnips without closing
+vim.keymap.set('n', '<leader><leader>s', '<Cmd>lua require("luasnip.loaders.from_lua").load { paths = "./snippets" }<CR>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
